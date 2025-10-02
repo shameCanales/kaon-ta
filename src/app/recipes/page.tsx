779d7721908de3224e6a8b29272e6857be753b12
shameCanales <canalesshame@gmail.com>
2025-10-02 13:57:44 +0800

    added initial styling for recipe page

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -3,6 +3,7 @@
 import { useGetRandomRecipes } from "@/lib/hooks/useGetRandomRecipes";
 import { Recipe } from "@/lib/types/recipe";
 import Image from "next/image";
+import { montserrat } from "@/lib/fonts";
 
 export default function Recipes() {
   const { data, isLoading, isError, error } = useGetRandomRecipes(5);
@@ -11,19 +12,19 @@ export default function Recipes() {
   if (isError) return <p>Error: {(error as Error).message}</p>;
 
   return (
-    <div>
-      <h1 className="text-2xl font-bold mb-4">Random Recipes</h1>
-      <ul className="grid grid-cols-2 gap-4">
+    <div className="px-6 py-5 mt-5">
+      <h1 className={` text-3xl font-extrabold ${montserrat.className}`}>Popular Recipes</h1>
+      <ul className="grid gap-4">
         {data?.map((recipe: Recipe) => (
-          <li key={recipe.id} className="p-4 border rounded-lg">
-            <p className="font-semibold">{recipe.title}</p>
+          <li key={recipe.id} className="shadow-2xs bg-stone-50 rounded-2xl">
             <Image
               src={recipe.image}
               alt={recipe.title}
-              width="50"
-              height="50"
-              className="mt-2 rounded"
+              width="550"
+              height="550"
+              className="rounded w-full aspect-video bg-clip-border"
             />
+            <p className="font-semibold">{recipe.title}</p>
           </li>
         ))}
       </ul>
--- src/lib/hooks/useGetRandomRecipes.ts
+++ src/lib/hooks/useGetRandomRecipes.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { getRandomRecipes } from "../services/getRandomRecipes";
 import { Recipe } from "../types/recipe";
 
-export function useGetRandomRecipes(number: number = 5) {
+export function useGetRandomRecipes(number: number = 20) {
   return useQuery<Recipe[]>({
     queryKey: ["randomRecipes", number],
     queryFn: () => getRandomRecipes(number),
